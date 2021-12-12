49ebcfe2706198e2181216979cb2d4eb6b5dfb8e
Adam Wonia <adam.wonia@gmail.com>
2021-12-12 16:42:27 +0100

    final changes

--- triangle.py
+++ triangle.py
@@ -1,10 +1,11 @@
 def triangle(a, b, c):
-    # The function returns True if with given sides length 'a', 'b', and 'c' 
-    # the triangle can be made and False otherwise
-    
+    """
+    This function checks whether a triangle can be formed from the given sides a, b and c.
+    :param a, b, c: The lengths of the sides of the triangle.
+    :return: Returns True if given sides form a triangle and False otherwise.
+    """
     # Determine the longest side - first element in the list:
     sides_table = sorted([a, b, c], key=None, reverse=True)
-
     # Check if the triangle can be made:
     if sides_table[0] < sides_table[1] + sides_table[2]:
         return True
